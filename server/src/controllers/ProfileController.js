8cd2d70625879bedc774e58e40916390669f4da8
Filipe Medeiro <medeirolp@gmail.com>
2022-11-19 01:53:47 -0300

    Rotas para buscar e atualizar perfil criadas

--- server/src/controllers/ProfileController.js
+++ server/src/controllers/ProfileController.js
@@ -1,12 +1,42 @@
-
+const UserProfile = require('../Models/UserProfile')
 
 class ProfileController {
-  async getProfile() {
+  async getProfile(req, res) {
+    const { id: userId } = req.user;
+
+    try {
+      const userProfile = await UserProfile.find({user_id: userId});
+
+      return res.status(200).json(userProfile)
+    } catch (error) {
+      return res.status(500).json({ message:  error.message});
+    }
+  }
+
+  async uploadAvatar() {
 
   }
 
-  async updateProfile() {
+  async updateProfile(req, res) {
+    const {id: userId} = req.user;
+    const {description, background_color, background_button_color, text_color } = req.body;
 
+    try {
+      const userUpdated = await UserProfile.updateMany({ user_id: userId }, {
+        description,
+        background_color,
+        background_button_color,
+        text_color
+      })
+
+      if(userUpdated.acknowledged === false){
+        return res.status(400).json({message: 'Não foi possível atualizar o perfil do usuário.'})
+      }
+      
+      return res.status(200).json({ message: 'Perfil atualizado com sucesso!'})
+    } catch (error) {
+      return res.status(500).json({ message:  error.message});
+    }
   }
 }
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const ProfileController = require('./controllers/ProfileController');
 const USersController = require('./controllers/USersController');
 const authentication = require('./middlewares/auth');
 
@@ -8,6 +9,10 @@ routes.post('/users', USersController.Create)
 routes.post('/users/login', USersController.Login)
 routes.post('/users/forgot', USersController.Forgot)
 routes.post('/users/reset/:code', USersController.Reset)
+
+routes.get('/profiles', authentication, ProfileController.getProfile);
+routes.post('/profiles/update', authentication, ProfileController.updateProfile);
+
 
 
 module.exports = routes;

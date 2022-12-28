900098073dbb7bb3e4582914d642f9e16634d19d
liverday <vitor.medeiro10@gmail.com>
2022-12-28 16:17:31 -0300

    feat: including mongodb

--- server/src/loaders/mongoDb.js
+++ server/src/loaders/mongoDb.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 async function startMongoDb() {
-  await mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true
-});
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+  }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 }
 
 module.exports = startMongoDb;
